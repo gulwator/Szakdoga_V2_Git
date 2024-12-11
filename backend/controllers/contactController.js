02cc5729c52f9ebff60dbab1e30ec547c4e5dacf
gulwator <toth.totya.gergo@gmail.com>
2024-12-11 23:34:10 +0100

    some backend formating

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,62 +1,61 @@
-
-const {db} = require("../dbConnection/dbConnection");
-const asyncHandler=require("express-async-handler");
+const { db } = require("../dbConnection/dbConnection");
+const asyncHandler = require("express-async-handler");
 
 // @desc Get all contracts
 //@route GET /api/contracts
 //@access public
 
-const getContacts = (req, res) => {
- let sql = `SELECT * FROM children`;
+const getContacts = asyncHandler(async (req, res) => {
+  let sql = `SELECT * FROM children`;
 
-  let contacts = db.all(sql,(error,rows)=> {
+  let contacts = db.all(sql, (error, rows) => {
     res.status(200).send(rows);
-
   });
   // console.log(contracts);
-};
+});
 
 // @desc Create new contracts
 //@route POST /api/contracts
 //@access public
 
 const createChild = asyncHandler(async (req, res) => {
-    const {
-      name,
-      dateOfbirth,
-      parantName,
-      parantPhone,
-      address,
-      schoolID,
-      color,
-      bandNumber,
-      illness,
-    } = req.body;
-    if (
-      !name ||
-      !dateOfbirth ||
-      !parantName ||
-      !parantPhone ||
-      !address ||
-      !schoolID
-    ) {
-      res.status(400);
-      throw new Error("all fields are reqired!");
-    }
-    const query =
-      "INSERT INTO children (name,dateOfBirth,parantName,parantPhone,address, schoolID, color, Bandnumber, illness) VALUES(?,?,?,?,?,?,?,?,?)";
-    const values = [
-      dateOfbirth,
-      parantName,
-      parantPhone,
-      address,
-      schoolID,
-      color,
-      bandNumber,
-      illness,
-    ];
-    db.run(query, values);
-    res.status(201).json({ message: "Child added to database" });
+  const {
+    name,
+    dateOfbirth,
+    parantName,
+    parantPhone,
+    address,
+    schoolID,
+    color,
+    bandNumber,
+    illness,
+  } = req.body;
+  if (
+    !name ||
+    !dateOfbirth ||
+    !parantName ||
+    !parantPhone ||
+    !address ||
+    !schoolID
+  ) {
+    res.status(400);
+    throw new Error("all fields are reqired!");
+  }
+  const query =
+    "INSERT INTO children (name,dateOfBirth,parantName,parantPhone,address, schoolID, color, Bandnumber, illness) VALUES(?,?,?,?,?,?,?,?,?)";
+  const values = [
+    name,
+    dateOfbirth,
+    parantName,
+    parantPhone,
+    address,
+    schoolID,
+    color,
+    bandNumber,
+    illness,
+  ];
+  db.run(query, values);
+  res.status(201).json({ message: "Child added to database" });
 });
 
 // @desc Get contract
@@ -66,14 +65,37 @@ const createChild = asyncHandler(async (req, res) => {
 const getContact = (req, res) => {
   res.status(200).json({ message: `Get Contact for ${req.params.id}` });
 };
-
 // @desc update contract
 //@route PUT /api/contract/:id
 //@access public
 
 const updateContact = (req, res) => {
-  try {
-  } catch (e) {}
+  const {
+    name,
+    dateOfbirth,
+    parantName,
+    parantPhone,
+    address,
+    schoolID,
+    color,
+    bandNumber,
+    illness,
+  } = req.body;
+  sql = `UPDATE children SET name = ?,dateOfBirth = ?,parantName = ?,parantPhone = ?,address = ?, schoolID = ?, color = ?, Bandnumber = ?, illness = ? WHERE id = ?`;
+  values = [
+    name,
+    dateOfbirth,
+    parantName,
+    parantPhone,
+    address,
+    schoolID,
+    color,
+    bandNumber,
+    illness,
+    req.params.id,
+  ];
+  db.run(sql, values);
+  res.status(201).json({ message: "child modified in database" });
   // res.status(200).json({ message: `Update Contact for ${req.params.id}` });
 };
 
