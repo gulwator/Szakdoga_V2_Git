c47d0821a62cb1c30dda09b99411ebf886396d2f
gulwator <toth.totya.gergo@gmail.com>
2024-12-12 13:08:46 +0100

    AddChild HTTP request working

--- frontend/camp-manager/src/components/AddChild.vue
+++ frontend/camp-manager/src/components/AddChild.vue
@@ -1,3 +1,122 @@
 <template>
-  <h3>Add Child Component</h3>
+  <div class="container m,t-4">
+    <div class="row">
+      <div class="col-md-6">
+        <form @submit.prevent="saveContact">
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.name"
+              class="form-control"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="date"
+              v-model="child.dateOfbirth"
+              class="form-control"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.address"
+              class="form-control"
+              placeholder="Address"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.color"
+              class="form-control"
+              placeholder="color"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.parantName"
+              class="form-control"
+              placeholder="Parent Name"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.parantPhone"
+              class="form-control"
+              placeholder="Parent Phone number"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.schoolID"
+              class="form-control"
+              placeholder="School"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.illness"
+              class="form-control"
+              placeholder="illness"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              v-model="child.bandNumber"
+              class="form-control"
+              placeholder="Band number"
+            />
+          </div>
+          <button type="submit" class="btn btn-dark btn-sm mt-4">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useToast } from "vue-toastification";
+
+const child = ref({
+  name: "",
+  dateOfbirth: "",
+  parantName: "",
+  parantPhone: "",
+  address: "",
+  schoolID: "",
+  color: "",
+  bandNumber: "",
+  illness: "",
+});
+const toast = useToast();
+const saveContact = async () => {
+  console.log("Child values:", child.value);
+  if (
+    !child.value.name ||
+    !child.value.dateOfbirth ||
+    !child.value.parantName ||
+    !child.value.parantPhone ||
+    !child.value.address ||
+    !child.value.schoolID ||
+    !child.value.illness
+  ) {
+    toast.error("Fields are required");
+  }
+  try {
+    const url = "http://localhost:3000/api/contacts";
+    const response = await axios.post(url, child.value);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
--- frontend/camp-manager/src/main.js
+++ frontend/camp-manager/src/main.js
@@ -1,6 +1,6 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -8,7 +8,7 @@ import "vue-router";
 import router from "./routes";
 
 const app = createApp(App);
-app.use(router);
+app.use(Toast).use(router);
 app.mount("#app");
 
 // createApp(App).mount("#app");
