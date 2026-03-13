83ca3b4db6c9bf1fd42d02e2fca48c2adb6bec2f
gulwator <toth.totya.gergo@gmail.com>
2026-03-13 09:06:54 +0100

    Update dbConnection.js

--- backend/dbConnection/dbConnection.js
+++ backend/dbConnection/dbConnection.js
@@ -1,8 +1,8 @@
 const createTables = require("./dbCreateTables.js");
 
 const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("./data.db", sqlite3.OPEN_READWRITE, (err) => {
+const dbData = process.env.DB_DATA || "./data.db";
+const db = new sqlite3.Database(dbData, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     return console.error(err.message);
   }
@@ -11,7 +11,13 @@ const db = new sqlite3.Database("./data.db", sqlite3.OPEN_READWRITE, (err) => {
 });
 try {
   db.run("PRAGMA foreign_keys = ON;");
-  createTables(db);
+  if (!db) {
+    throw new Error("Failed to initialize database");
+  }
+  if (process.env.NODE_ENV === "development") {
+    console.log("Running in development mode, creating tables...");
+    createTables(db);
+  }
 } catch (error) {
   console.error("Error creating tables:", error.message);
 }
